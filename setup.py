0ff461671adab3ec14af3b8e69e0966594e715f5
Ankit Show <ankitshow005@gmail.com>
2026-01-03 19:05:20 +0530

    Project structure added with venv set up

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ankit Show",
+    author_email="showankit@gmail.com",
+    packages=find_packages()
+)
